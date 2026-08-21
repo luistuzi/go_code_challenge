cbc9e3ff7f491b67df51d573d114ec31f949fd25
luistuzi <luistonha@gmail.com>
2026-08-21 09:59:56 -0300

    Inserting connection with database

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"go_code_challenge/cmd/api"
+	"go_code_challenge/config"
+	"go_code_challenge/db"
 	"log"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+
+	db, err := db.NewSQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
